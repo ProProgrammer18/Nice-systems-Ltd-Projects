bb059a99476ab06e710bf13becb73c6f7bd9f3fe
vikas <vika@gmail.com>
2023-01-10 23:45:02 +0530

    front-end code

---
+++ Performance Dashboard/front-end/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+// import Graph from "./components/basics/Graph";
+// import ReactDOM from "react-dom";
+import UploadFile from './components/basics/UploadFile';
+
+
+function App() {
+  return (
+    <>
+      {/* <h1>Hello demo</h1> */}
+      {/* <Graph /> */}
+      <UploadFile />
+     
+    </>
+  );
+}
+
+export default App;
---
+++ Performance Dashboard/front-end/src/components/basics/UploadFile.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import './UploadFile.css'
+
+const UploadFile = () => {
+  const [file, setFile] = useState(''); // storing the uploaded file
+  const SubmitButton = async () => {
+    try {
+      if (file) {
+        const formData = new FormData();
+        formData.append('logfile', file);
+        const res = await axios.post(
+          'http://localhost:5000/insert',
+          formData,
+        );
+        if (res.status === 200) {
+          alert("File Uploaded Successfully");
+        }
+        else if (res.status === 201) {
+          alert("File already exists")
+        }
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <form action='#'>
+        <div id="loginform">
+          <h2 id="headerTitle">Upload your File</h2>
+          <div>
+            <div className="row">
+              <input type="file" placeholder="Upload your file" onChange={(e) => setFile(e.target.files[0])} />
+            </div>
+            <div id="button" className="row">
+              <button onClick={SubmitButton}>Submit</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default UploadFile
